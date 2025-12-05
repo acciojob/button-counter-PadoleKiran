ecb8e5ca9ca3be29fd16b35eb44a94ab3e8ca75d
Kiran <padolekiran2001@address.com>
2025-12-05 11:40:05 +0530

    task compleated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [val,setVal] = useState(0);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Button program</h1>
+        <p>Button clicked {val} times</p>
+        <button onClick={() => setVal(val+1)}>Click me</button>
     </div>
   )
 }
